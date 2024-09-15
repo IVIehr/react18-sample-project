4697c3faf890acc210b8a8aaff12ece2d6668559
Mehrnoosh Navidimehr <m.navidimehr@dotin.ir>
2024-09-15 15:07:58 +0330

    feat: implement day and night theme

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,42 @@
-import { BsCart3, BsMoonFill, BsSunFill } from "react-icons/bs";
-import { FaBarsStaggered } from "react-icons/fa6";
-import { HiOutlineHome } from "react-icons/hi2";
+import { useState, useEffect } from "react";
+
 import { NavLink } from "react-router-dom";
+
+import { HiOutlineHome } from "react-icons/hi2";
+import { FaBarsStaggered } from "react-icons/fa6";
+import { BsCart3, BsMoonFill, BsSunFill } from "react-icons/bs";
+
 import NavLinks from "./NavLinks";
 
+const themes = {
+  winter: "winter",
+  dracula: "dracula",
+};
+
+const getThemeFromLocalStorage = () => {
+  return localStorage.getItem("theme") || themes.winter;
+};
+
 const Navbar = () => {
+  const [theme, setTheme] = useState(getThemeFromLocalStorage());
+
+  const handleTheme = () => {
+    const { winter, dracula } = themes;
+    const newTheme = theme === winter ? dracula : winter;
+    setTheme(newTheme);
+  };
+
+  useEffect(() => {
+    document.documentElement.setAttribute("data-theme", theme);
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+
   return (
     <nav className="bg-base-200">
       <div className="navbar align-element ">
         <div className="navbar-start">
           <NavLink to="/" className="hidden lg:flex text-3xl items-center ">
-            <HiOutlineHome className="text-black h-6 w-6" />
+            <HiOutlineHome className=" h-6 w-6" />
           </NavLink>
           <div className="dropdown">
             <label tabIndex={0} className="btn btn-ghost lg:hidden">
@@ -38,6 +64,13 @@ const Navbar = () => {
               </span>
             </div>
           </NavLink>
+        </div>
+        <div className="navbar-end">
+          <label className="swap swap-rotate ">
+            <input type="checkbox" onChange={handleTheme} />
+            <BsSunFill className="swap-on h-4 w-4" />
+            <BsMoonFill className="swap-off h-4 w-4" />
+          </label>
         </div>
       </div>
     </nav>
